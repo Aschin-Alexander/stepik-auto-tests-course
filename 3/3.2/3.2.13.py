734556937648293822d834c066f9e6f9c0a2fc7e
Aschin-Alexander <astschin@mail.ru>
2021-03-22 08:31:02 +0300

    learn unittest correct test

--- 3/3.2/3.2.13.py
+++ 3/3.2/3.2.13.py
@@ -6,6 +6,45 @@ from selenium.webdriver.common.by import By
 
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CSS_SELECTOR, "div.first_block .first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, "div.first_block .second")
+        input2.send_keys("Ivanov")
+        input3 = browser.find_element(By.CSS_SELECTOR, "div.first_block .third")
+        input3.send_keys("Ivanov@ivan.ru")
+        input4 = browser.find_element(By.CSS_SELECTOR, "div.second_block .first")
+        input4.send_keys("88005553535")
+        input5 = browser.find_element(By.CSS_SELECTOR, "div.second_block .second")
+        input5.send_keys("far far away")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        #time.sleep(5)
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_abs2(self):
         link = "http://suninjuly.github.io/registration2.html"
         browser = webdriver.Chrome()
         browser.get(link)
@@ -24,7 +63,7 @@ class TestAbs(unittest.TestCase):
 
         # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button.btn")
-        time.sleep(10)
+        #time.sleep(5)
         button.click()
 
         # Проверяем, что смогли зарегистрироваться
@@ -37,11 +76,13 @@ class TestAbs(unittest.TestCase):
         welcome_text = welcome_text_elt.text
 
         # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "missing edit")
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
 
-        time.sleep(6)
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(5)
         # закрываем браузер после всех манипуляций
         browser.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
